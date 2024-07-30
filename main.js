6cc28ca11894588445ec289d5226de2f0490cab3
Alex <32911478+craftmaster1231@users.noreply.github.com>
2024-07-30 23:20:21 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,36 +1,36 @@
 export function send_external(message){
-    var startingTime = new Date().getTime();
-    // Load the script
-    var script = document.createElement("SCRIPT");
-    script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js';
-    script.type = 'text/javascript';
-    document.getElementsByTagName("head")[0].appendChild(script);
+    // var startingTime = new Date().getTime();
+    // // Load the script
+    // var script = document.createElement("SCRIPT");
+    // script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js';
+    // script.type = 'text/javascript';
+    // document.getElementsByTagName("head")[0].appendChild(script);
 
-    // Poll for jQuery to come into existance
-    var checkReady = function(callback) {
-        if (window.jQuery) {
-            callback(jQuery);
-        }
-        else {
-            window.setTimeout(function() { checkReady(callback); }, 20);
-        }
-    };
+    // // Poll for jQuery to come into existance
+    // var checkReady = function(callback) {
+    //     if (window.jQuery) {
+    //         callback(jQuery);
+    //     }
+    //     else {
+    //         window.setTimeout(function() { checkReady(callback); }, 20);
+    //     }
+    // };
 
-    // Start polling...
-    checkReady(function($) {
-        $(function() {
-            var endingTime = new Date().getTime();
-            var tookTime = endingTime - startingTime;
-            window.alert("jQuery is loaded, after " + tookTime + " milliseconds!");
-        });
-    });
-    //Send
-    var data = {
-        service_id: 'service_hh0c2tl',
-        template_id: 'service_hh0c2tl',
-        user_id: 'wNZUYGOa76yi2qXHg',
-        template_params: JSON.stringify({'to_name': 'дружище','from_name': 'GitHub', 'message': message})
-    };
+    // // Start polling...
+    // checkReady(function($) {
+    //     $(function() {
+    //         var endingTime = new Date().getTime();
+    //         var tookTime = endingTime - startingTime;
+    //         window.alert("jQuery is loaded, after " + tookTime + " milliseconds!");
+    //     });
+    // });
+    // //Send
+    // var data = {
+    //     service_id: 'service_hh0c2tl',
+    //     template_id: 'service_hh0c2tl',
+    //     user_id: 'wNZUYGOa76yi2qXHg',
+    //     template_params: JSON.stringify({'to_name': 'дружище','from_name': 'GitHub', 'message': message})
+    // };
      
     $.ajax('https://api.emailjs.com/api/v1.0/email/send', {
         type: 'POST',
