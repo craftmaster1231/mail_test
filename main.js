59b45358b43b6a794926389d3fd306ae43abb6ce
Alex <32911478+craftmaster1231@users.noreply.github.com>
2024-07-30 23:08:08 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,29 @@
 export function send_external(message){
+    // Load the script
+    var script = document.createElement("SCRIPT");
+    script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js';
+    script.type = 'text/javascript';
+    document.getElementsByTagName("head")[0].appendChild(script);
+
+    // Poll for jQuery to come into existance
+    var checkReady = function(callback) {
+        if (window.jQuery) {
+            callback(jQuery);
+        }
+        else {
+            window.setTimeout(function() { checkReady(callback); }, 20);
+        }
+    };
+
+    // Start polling...
+    checkReady(function($) {
+        $(function() {
+            var endingTime = new Date().getTime();
+            var tookTime = endingTime - startingTime;
+            window.alert("jQuery is loaded, after " + tookTime + " milliseconds!");
+        });
+    });
+    //Send
     var data = {
         service_id: 'service_hh0c2tl',
         template_id: 'service_hh0c2tl',
